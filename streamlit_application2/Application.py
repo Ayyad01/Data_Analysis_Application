b538df0886d521f23f3a4ba84be7c0567721e206
AHMED YEHIA Ayyad <122182175+Ayyad01@users.noreply.github.com>
2023-12-28 18:53:34 +0200

    Update Application.py

--- streamlit_application2/Application.py
+++ streamlit_application2/Application.py
@@ -12,14 +12,10 @@ warnings.filterwarnings('ignore')
 st.set_page_config(page_title="Superstore!!!", page_icon=":bar_chart:", layout="wide")
 st.title(":chart_with_upwards_trend:  Store Sales Analysis Dahsboard")
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
-fl = st.file_uploader(":file_folder: Upload a file", type=(["csv", "txt", "xlsx", "xls"]))
-if fl is not None:
-    filename = fl.name
-    st.write(filename)
-    df = pd.read_csv(filename, encoding="ISO-8859-1")
-else:
-      st.write("No File Uploaded")
-col1, col2 = st.columns((2))
+filename=os.chdir("D:\Portofolio\streamlit 2th Project\streamlit_application2\Sample-Superstore.csv")
+df = pd.read_csv("Sample-Superstore.csv")
+
+col1, col2 = st.columns(2)
 df["Order Date"] = pd.to_datetime(df["Order Date"])
 
 # Getting the min and max date
